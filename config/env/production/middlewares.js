3d49d13f55e2fbd72c156db70eded2a631fb3241
RadekMlada <radek.mlada@gmail.com>
2022-02-15 19:11:59 +0100

    .

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,17 @@
+[
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  {
+    name: 'strapi::public',
+    config: {
+      path: '/storage/public'
+    }
+  }
+];
