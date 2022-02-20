94eb1260994dd254a1aa816233470cf94d0032fa
RadekMlada <radek.mlada@gmail.com>
2022-02-20 17:51:02 +0100

    Update middlewares.js

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,6 +1,16 @@
-[
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-eval'", 'ageproject.radekmlada.com', '*.googleapis.com', '*.gstatic.com'],
+          'img-src': ["'self'", 'data:', 'ageproject.radekmlada.com', 'strapi.io', 'maps.gstatic.com', '*.googleapis.com', '*.ggpht.com'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -8,5 +18,5 @@
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public'
+  'strapi::public',
 ];
