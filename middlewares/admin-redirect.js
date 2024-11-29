b0d0d2a98ec91f11cde77f0b4b0a855c345f127a
RadekMlada <radek.mlada@gmail.com>
2024-11-29 21:46:23 +0100

    snad ted

--- middlewares/admin-redirect.js
+++ middlewares/admin-redirect.js
@@ -2,7 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (_config, { strapi }) => {
-  const redirects = ['/', '/index.html'].map((routePath) => ({
+  const redirects = ['ageprojectadmin:1337',
+    'ageprojectadmin:1337/', 
+    'http://ageprojectadmin:1337',
+    'http://ageprojectadmin:1337/', 
+    'https://ageprojectadmin:1337',
+    'https://ageprojectadmin:1337/', 
+    'ageproject.cz/',
+    'ageproject.cz',
+    'https://ageproject.cz/',
+    'https://ageproject.cz',
+    '/',
+    '/index.html'].map((routePath) => ({
     method: 'GET',
     path: routePath,
     handler: (ctx) => {
